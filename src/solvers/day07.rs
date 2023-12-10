a8232bc0e51a04a807eebc0fdc70dcbc13467b9e
Peter <peterbecker89@proton.me>
2023-12-10 16:01:20 -0700

    solve day 7 star 1

--- src/solvers/day07.rs
+++ src/solvers/day07.rs
@@ -1,9 +1,10 @@
 use crate::Solver;
+use itertools::Itertools;
 
 #[derive(Debug)]
 struct CardHand {
-    hand: Vec<u32>,
-    bid: u32,
+    hand: Vec<u64>,
+    bid: u64,
 }
 
 impl CardHand {
@@ -13,7 +14,7 @@ impl CardHand {
         let hand = hand
             .chars()
             .map(|card| match card {
-                '2'..='9' => card.to_digit(10).unwrap() as u32,
+                '2'..='9' => card.to_digit(10).unwrap() as u64,
                 'T' => 10,
                 'J' => 11,
                 'Q' => 12,
@@ -23,9 +24,42 @@ impl CardHand {
             })
             .collect();
 
-        let bid = bid.parse::<u32>().unwrap();
+        let bid = bid.parse::<u64>().unwrap();
 
         CardHand { hand, bid }
+    }
+
+    fn score(&self) -> u64 {
+        let counts = self.hand.iter().counts();
+
+        let hand_type_bonus: u64 = match counts.len() {
+            1 => 6_000_000_000,
+            2 => {
+                if counts.values().contains(&4) {
+                    5_000_000_000
+                } else {
+                    4_000_000_000
+                }
+            }
+            3 => {
+                if counts.values().contains(&3) {
+                    3_000_000_000
+                } else {
+                    2_000_000_000
+                }
+            }
+            4 => 1_000_000_000,
+            _ => 0,
+        };
+
+        let hand_score = self
+            .hand
+            .iter()
+            .enumerate()
+            .map(|(index, value)| value << (4 * (4 - index)))
+            .sum::<u64>();
+
+        hand_type_bonus + hand_score
     }
 }
 
@@ -33,7 +67,14 @@ pub struct Day07;
 
 impl Solver for Day07 {
     fn star_one(&self, input: &str) -> String {
-        todo!()
+        input
+            .lines()
+            .map(CardHand::from)
+            .sorted_by_key(|hand| hand.score())
+            .enumerate()
+            .map(|(index, hand)| hand.bid * (index as u64 + 1))
+            .sum::<u64>()
+            .to_string()
     }
 
     fn star_two(&self, input: &str) -> String {
